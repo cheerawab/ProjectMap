f29f59af563f0ddccb00ec760fb4a816c2b4c023
Cheer_awab <cheerawab@gmail.com>
2024-12-29 06:08:28 +0000

    api

--- ProjectMap/project_map/__init__.py
+++ ProjectMap/project_map/__init__.py
@@ -1,4 +1,4 @@
-from mcdrefoged.api.command import SimpleCommandBuilder, Integer, Text, GreedyText
+from mcdrefoged.api.all import *
 
 def on_load(server: PluginServerInterface, pre_module):
     server.logger.info("ProjectMap已加載")
